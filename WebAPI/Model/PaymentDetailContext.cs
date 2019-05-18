0e6b7c71b72a82d925029cc7506cd6d2f82c80bd
somic723 <afsharan.h@gmail.com>
2019-05-18 09:35:06 +0430

    Copy Project

---
+++ WebAPI/Model/PaymentDetailContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Model
+{
+    public class PaymentDetailContext: DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options)
+            :base(options)
+        {
+            //in options we have to pass information like:
+                //DB provider like sql server, mySql
+                //connection string for the DB
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
